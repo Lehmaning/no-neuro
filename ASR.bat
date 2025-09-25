a96f9a4a4ecf5b629b6794c9fa90e0ba6e4edd00
Lehmaning <60032845+Lehmaning@users.noreply.github.com>
2025-09-26 00:05:30 +0800

    chore: use uv to start cli services

--- ASR.bat
+++ ASR.bat
@@ -1,6 +1,6 @@
 @echo off
 :: 使用脚本所在目录作为工作目录
 cd %~dp0
-call conda activate my-neuro && python asr_api.py
+call uv run python asr_api.py
 pause
 
--- bert.bat
+++ bert.bat
@@ -1,5 +1,5 @@
 @echo off
 :: 使用脚本所在目录作为工作目录
 cd /d %~dp0
-call conda activate my-neuro && python omni_bert_api.py
+call uv run python omni_bert_api.py
 pause
